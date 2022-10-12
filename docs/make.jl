b38dea567b8cbe101b346fe8f00a8df08b50a775
mvanzulli <mvanzulli@fing.edu.uy>
2022-10-12 17:53:41 -0300

    added docs :book:

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter
+using Apolo
+
+makedocs(
+    sitename = "Apolo",
+    format = Documenter.HTML(),
+    modules = [Apolo]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
---
+++ docs/src/index.md
@@ -0,0 +1,3 @@
+# Apolo.jl
+
+Documentation for Apolo.jl
