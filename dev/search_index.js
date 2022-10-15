82869d042a8f42cc9366e32a44cbcfcc0ed63c32
Documenter.jl <documenter@juliadocs.github.io>
2022-10-15 00:53:36 +0000

    build based on 42d47c2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Apolo.jl","page":"Apolo.jl","title":"Apolo.jl","text":"","category":"section"},{"location":"","page":"Apolo.jl","title":"Apolo.jl","text":"Documentation for Apolo.jl","category":"page"}]
+}
