9e30f5dedcf82a4c36921153bc3c87c3b389c19d
mvanzulli <mvanzulli@fing.edu.uy>
2022-10-17 19:30:02 -0300

    working on image functors and tests :picture:

--- src/Interfaces/Images.jl
+++ src/Interfaces/Images.jl
@@ -7,65 +7,198 @@ module Images
 # Import external dependencies to overload
 import Statistics: mean, std
 import Base: size, length
+
 # Import local dependencies to overload
-import ..ForwardProblem: getgrid, getdim
+import ..ForwardProblem: dimension, getgrid
 
 # Add libraries to use
+using AutoHashEquals
 using DICOM: DICOMData, dcmdir_parse, @tag_str
-using SparseArrays: spzeros
+using Ferrite: Grid, Quadrilateral, DofHandler, get_point_values
+using Apolo: create_grid
 
 # Export interface functions and types
-export AbstractImage, MedicalImage, MedicalSegment,
-    load_dicom, getintensity, getnumpix,
-    getspacing, gethyp, getorientation, 
-    get_patient_name, getorigin, build_coordinates,
-    getindexes, getnumpix, mean,
-    std, getvarcoef, compute_histogram
+export AbstractImage, MedicalImage, MedicalSegment, GenericImage,
+    # Extractors AbstractImage
+    intensity, intensity_type, numpix,
+    spacing, start, finish, length, coordinates,
+    # Features AbstractImage 
+    create_img_grid,
+    # Extractors MedicalImage
+    hyper_params, orientation, patient_name, indexes,
+    # Medical image features
+    mean, load_dicom, std, getvarcoef, compute_histogram
 
 
 # Scalar parameters for the interface
 const ORIENTATION_TOLERANCE = 1e-2 # this parameter is the tolerance to not consider RAS to IJK rotation
 
-#TODO: med_img(2,2,4) en pixeles
-# med_img[1,2,3] = 
-
 """ Abstract supertype for all images.
 
 The following methods are provided by the interface:
 
-- `getintensity(img)`      -- returns the intensity array of the image `img`. 
-- `get_intensity_type(img)`-- returns the intensity data type.
-- `getnumpix(img)`         -- returns the image resolution in pixels. 
-- `getspacing(img)`        -- returns the space between pixels. 
-- `getorigin(img)`         -- returns the image origin coordinates. 
-- `getlength(img)`         -- returns the image dimensions in metric units.
+- `intensity(img)`         -- returns the intensity array of the image `img`. 
+- `intensity_type(img)`    -- returns the intensity data type.
+- `numpix(img)`            -- returns the image resolution in pixels. 
+- `spacing(img)`           -- returns the space between pixels. 
+- `start(img)`             -- returns the start point coordinates. 
+- `finish(img)`            -- returns the finish point coordinates.
+- `length(img)`            -- returns the image dimensions in metric units.
+- `coordinates(img)`       -- returns the image coordinates in three linear ranges.
+- `grid(img)`              -- returns the image grid.
 """
-abstract type AbstractImage{T,D} end 
+abstract type AbstractImage{T,D} end
 
 ## Methods for abstract parameters:
 " Gets the image intensity array"
-getintensity(img::AbstractImage) = img.intensity
+intensity(img::AbstractImage) = img.intensity
 
 " Gets the image dimension"
-getdim(::AbstractImage{T,D}) where {T,D} = D
+dimension(::AbstractImage{T,D}) where {T,D} = D
 
 " Gets the image intensity array data type"
-get_intensity_type(::AbstractImage{T,D}) where {T,D} = T
+intensity_type(::AbstractImage{T,D}) where {T,D} = T
 
 " Gets the image size"
-size(img::AbstractImage) = size(getintensity(img))
+size(img::AbstractImage) = size(intensity(img))
 
 " Gets the image dimensions in voxels"
-getnumpix(img::AbstractImage) = img.dimension
+numpix(img::AbstractImage) = img.num_pixels
 
 " Gets the image spacing between pixels"
-getspacing(img::AbstractImage) = img.spacing
+spacing(img::AbstractImage) = img.spacing
 
-" Gets the image origin"
-getorigin(img::AbstractImage) = img.origin
+" Gets the image start point"
+start(img::AbstractImage) = img.start
 
 " Gets the image metric dimensions"
-getlength(img::AbstractImage) = size(img) .* collect(getspacing(img)) 
+length(img::AbstractImage) = size(img) .* spacing(img)
+
+" Gets the image finish point"
+finish(img::AbstractImage) = start(img) .+ length(img)
+
+" Gets the image grid"
+getgrid(img::AbstractImage) = img.grid
+
+" Gets the image coordinates"
+function coordinates(img::AbstractImage)
+
+    # get start (start point)
+    start_img = start(img)
+    # get spacing
+    spacing_img = spacing(img) |> collect
+    # get length 
+    length_img = length(img)
+    # get number of pixels 
+    num_pixels = numpix(img) |> collect
+    # compute finish 
+    finish_img = start_img .+ length_img
+
+
+    # build start and finish point of the pixels grid
+    X_begin = start_img .+ spacing_img ./ 2
+    X_end = finish_img .- spacing_img ./ 2
+
+    # build coordinates 
+    coords = LinRange.(X_begin, X_end, num_pixels)
+
+    return coords
+end
+
+"Creates mesh grid inside the image frame"
+function create_img_grid(
+    start_img::NTuple{D,<:Real},
+    spacing_img::NTuple{D,<:Real},
+    length_img::NTuple{D,<:Real},
+    num_pixels::NTuple{D,<:Integer},
+) where {D}
+
+    # Image parameters
+    finish_img = start_img .+ length_img
+
+    # Mesh parameters
+    num_elements = num_pixels .- Tuple(ones(Int,D))
+    # compute start point
+    start_mesh = start_img .+ spacing_img ./ 2
+    # compte finish point
+    finish_mesh = finish_img .- spacing_img ./ 2
+
+    # generate grid
+    grid = create_grid(2, num_elements, start_mesh, finish_mesh, Quadrilateral)
+
+    return grid
+end
+
+
+""" Generic image struct.
+
+### Fields:
+
+- `intensity`  -- Intensity array
+- `num_pixels` -- Number of pixels in each direction
+- `spacing`    -- Space in each direction between each pixel
+- `start`      -- Start coordinates (considering the start located at [1,1,1] )
+- `grid`       -- Ferrite grid inside the image
+"""
+struct GenericImage{T,D} <: AbstractImage{T,D}
+    intensity::Array{T,D}
+    num_pixels::NTuple{D,<:Integer}
+    spacing::NTuple{D,<:Real}
+    start::NTuple{D,<:Real}
+    grid::Grid
+    # Cash the Ferrite.Grid into grid field of MedicalImage
+    function GenericImage(
+        intensity::Array{T,D},
+        num_pixels::NTuple{D,<:Integer},
+        spacing::NTuple{D,<:Real},
+        start::NTuple{D,<:Real},
+        ) where {T,D}
+        
+        # compute end point
+        finish = start .+ num_pixels .* spacing
+        length_img = finish .- start
+        # create grid
+        grid = create_img_grid(start, spacing, length_img, num_pixels)
+        
+        # instantiate generic grid 
+        new{T,D}(intensity, num_pixels, spacing, start, grid)
+    end
+end
+
+(img::AbstractImage{T,2})(x, y, z) = _eval_intenisty(img,(x, y)) 
+(img::AbstractImage{T,3})(x, y, z) = # llamada al eval handler 
+
+function _eval_intenisty(img::AbstractImage{T,2}, p::NTuple{2, <:Real}) where T 
+
+    #TODO: Increase performance of this operation
+    # get the image intensity and transform it into a vector
+    int_ferrite_vec = Vector{Float64}()
+
+    for intᵢ in vec(intensity(img))
+        push!(int_ferrite_vec, intᵢ)
+    end
+
+    # create dof handler
+    grid_img = getgrid(img)
+    dh_img = DofHandler(grid_img)
+
+    # push intensity field
+    push!(dh_img, :intensity, 1, Lagrange{2,RefCube,1}())
+    close!(dh_img)
+
+    # create evaluation handlar
+    eval_point = [Vec(p)]
+    ph_img = PointEvalHandler(grid_img, eval_point);
+
+    # 
+    i_points = Ferrite.get_point_values(
+        ph_img, dh_img, 
+        int_ferrite_vec,
+        :intensity
+        )
+
+    return i_points
+end
 
 
 """ Medical image struct.
@@ -75,18 +208,29 @@ getlength(img::AbstractImage) = size(img) .* collect(getspacing(img))
 - `intensity`   -- intensity array
 - `dimension`   -- number of pixels in each direction
 - `spacing`     -- space in each direction between each pixel
-- `origin`      -- origin coordinates (considering the origin located at [1,1,1] )
+- `start`      -- start coordinates (considering the start located at [1,1,1] )
 - `orientation` -- patient orientation tolerance ORIENTATION_TOLERANCE 
 - `hyp_params`  -- last slice DICOM image with its corresponding hyper parameters 
 
 """
-struct MedicalImage{T,D,F} <:AbstractImage{T,D}
+struct MedicalImage{T,D,F} <: AbstractImage{T,D}
     intensity::Array{T,D}
     dimension::NamedTuple
     spacing::NamedTuple
-    origin::NTuple{D,<:Real}
+    start::NTuple{D,<:Real}
     orientation::Symbol
     hyp_params
+    grid::Grid
+    # Cash the Ferrite.Grid element of the MedicalImage
+    # function MedicalImage{T,D,F}(
+    #     intensity::Array{T,D}
+    #     dimension::NamedTuple
+    #     spacing::NamedTuple
+    #     start::NTuple{D,<:Real}
+    #     orientation::Symbol
+    #     hyp_params) where {T,D,F} 
+
+    # end
 end
 #AbstractImage{D}
 #TODO: Fake Image 
@@ -96,44 +240,20 @@ end
 # (med_img::MedicalImage)(x, y, z) = # llamada al eval handler 
 
 " Gets the image hyper-parameters"
-getdim(med_img::MedicalImage) = med_img.dimension
+dimension(med_img::MedicalImage) = med_img.dimension
 " Gets the image hyper-parameters"
-gethyp(med_img::MedicalImage) = med_img.hyp_params
+hyper_parameter(med_img::MedicalImage) = med_img.hyp_params
 
 
-" Gets the image coordinates"
-function build_coordinates(med_img::MedicalImage{T,2}) where T
-    
-    # get origin
-    (oₓ, oⱼ, oₖ)  = getorigin(med_img)
-    # get spacing
-    (Δₓ, Δⱼ, Δₖ)  = getspacing(med_img) |> collect
-    # get length 
-    (Lₓ, Lⱼ, Lₖ) = getlength(med_img)
-    # get length 
-    (nₓ, nⱼ, nₖ) = getnumpix(med_img) |> collect
-    
-    # build start and finish 
-    x_begin = oₓ + Δₓ/2; x_end =  Lₓ - Δₓ/2
-    y_begin = oⱼ + Δⱼ/2; y_end =  Lⱼ - Δⱼ/2
-    z_begin = oₖ + Δₖ/2; z_end =  Lₖ - Δₖ/2
-    
-    # build coordinates 
-    x_coords = LinRange(x_begin, x_end, nₓ)
-    y_coords = LinRange(y_begin, y_end, nⱼ)
-    z_coords = LinRange(z_begin, z_end, nₖ)
-
-    return x_coords, y_coords, z_coords
-end
 "Gets the patient name"
-get_patient_name(med_img::MedicalImage) = gethyp(med_img)[tag"PatientName"]
+patient_name(med_img::MedicalImage) = hyper_parameter(med_img)[tag"PatientName"]
 
 "Extracts the pixel by its index"#TODO
 # Base.getindex(m_img::MedicalImage, i1::Int, i2::Int, i3::Int) = m_img.intensity[i1, i2, i3]
 
 
 " Extract the pixel data form a DICOM array"
-function getpixeldata(dcm_array)
+function pixeldata(dcm_array)
     if length(dcm_array) == 1
         return only(dcm_array).PixelData
     else
@@ -141,15 +261,15 @@ function getpixeldata(dcm_array)
     end
 end
 
-function getorientation(dcm::MedicalImage{T,D,DICOMData}) where {T,D}
+function orientation(dcm::MedicalImage{T,D,DICOMData}) where {T,D}
     # extract DICOM hyper parameters
-    hyp = gethyp(dcm)
+    hyp = hyper_parameter(dcm)
     # run 
-    orient = getorientation(hyp)
+    orient = orientation(hyp)
     return orient
 end
 "Extract the image orientation from a DICOMData"
-function getorientation(dcm::DICOMData)
+function orientation(dcm::DICOMData)
     orientations = Dict(
         :coronal => [1, 0, 0, 0, 0, -1],
         :sagittal => [0, 1, 0, 0, 0, -1],
@@ -166,7 +286,7 @@ function getorientation(dcm::DICOMData)
 end
 
 "Returns a named tuple with image spacing in axial, sagital and coronal direction "
-getspacing(dcm) = (sagital=dcm[tag"PixelSpacing"][1], coronal=dcm[tag"PixelSpacing"][2], axial=dcm[tag"SliceThickness"])
+spacing(dcm) = (sagital=dcm[tag"PixelSpacing"][1], coronal=dcm[tag"PixelSpacing"][2], axial=dcm[tag"SliceThickness"])
 
 " Loads a DICOM directory and returns a named tuple array of different series "
 function load_dicom(dir::String)
@@ -180,18 +300,18 @@ function load_dicom(dir::String)
     # extract the image type
     format = typeof(dcm)
     # extract the intensity array
-    intensity = getpixeldata(dcms)
+    intensity = pixeldata(dcms)
     # extract the intensity array data type
     intensity_type = eltype(intensity)
     # extract the image spacing 
-    spacing = getspacing(dcm)
+    spacing = spacing(dcm)
     # extract the image dimension 
     dimension = (sagital=size(intensity, 1), coronal=size(intensity, 2), axial=size(intensity, 3))
     dim_number = length(dimension)
     # extract the image orientation
-    orientation = getorientation(dcm)
+    orientation = orientation(dcm)
     # construct and build the image 
-    med_img = MedicalImage{intensity_type, dim_number, format}(intensity, dimension, spacing, (0, 0, 0), orientation, dcm)
+    med_img = MedicalImage{intensity_type,dim_number,format}(intensity, dimension, spacing, (0, 0, 0), orientation, dcm)
     return med_img
 end
 
@@ -213,34 +333,34 @@ end
 
 
 " Gets medical image indexes"
-getindexes(med_seg::MedicalSegment) = med_seg.indexes
+indexes(med_seg::MedicalSegment) = med_seg.indexes
 " Gets the number of pixels inside an image segment"
-getnumpix(med_seg::MedicalSegment) = med_seg.num_pix
+num_pixels(med_seg::MedicalSegment) = med_seg.num_pix
 " Gets the image segment intensity array"
-getintensity(med_seg::MedicalSegment) = med_seg.intensity_vec
+intensity(med_seg::MedicalSegment) = med_seg.intensity_vec
 
 "Computes the histdogram given a vector of intensity edges"
 function compute_histogram(
     med_img::MedicalImage,
     edges::Vector{<:Number},
-    ) 
+)
     # get the intensity array
-    intensity = getintensity(med_img)
+    intensity = intensity(med_img)
 
     # sort edges min to max value
     edges = sort(edges)
 
     # check edges are inside the image range 
-    (edgeₘᵢₙ, edgeₘₐₓ) = extrema(edges) 
-    (intensityₘᵢₙ, intensityₘₐₓ) = extrema(intensity) 
-    inside_bool = edgeₘᵢₙ ≥ intensityₘᵢₙ &&  edgeₘₐₓ ≤ intensityₘₐₓ 
+    (edgeₘᵢₙ, edgeₘₐₓ) = extrema(edges)
+    (intensityₘᵢₙ, intensityₘₐₓ) = extrema(intensity)
+    inside_bool = edgeₘᵢₙ ≥ intensityₘᵢₙ && edgeₘₐₓ ≤ intensityₘₐₓ
     !inside_bool && throw(BoundsError("intensity edges must be inside img_int"))
 
     # number of  edges
     num_segments = length(edges) + 1
 
     # initialize
-    segments = Vector{MedicalSegment}(undef, num_segments )
+    segments = Vector{MedicalSegment}(undef, num_segments)
 
     # compute the number of voxels that are inside the borders
     for segment_idx in 1:num_segments
@@ -248,9 +368,9 @@ function compute_histogram(
         # left
         borderₘᵢₙ = if segment_idx == 1
             intensityₘᵢₙ
-        else      
-            edges[segment_idx - 1]
-        end 
+        else
+            edges[segment_idx-1]
+        end
         #right
         borderₘₐₓ = if segment_idx == num_segments
             intensityₘₐₓ
@@ -262,19 +382,19 @@ function compute_histogram(
 
         # compute the cartesian indexes of pixels that satisfy is_in_segment condition  
         indexes_segment = findall(is_in_segment, intensity)
-        
+
         # compute the intensity segment array 
         intensity_segment = intensity[indexes_segment]
 
         # number of pixels inside the segment
         counts_segment = length(indexes_segment)
 
-       # build segment 
-        segment_obj = 
-        MedicalSegment(
-            indexes_segment,
-            counts_segment,
-            intensity_segment,
+        # build segment 
+        segment_obj =
+            MedicalSegment(
+                indexes_segment,
+                counts_segment,
+                intensity_segment,
             )
 
         # fill segments vector
@@ -286,9 +406,9 @@ end
 
 
 " Gets the image segment intensity mean"
-mean(med_seg::MedicalSegment) = mean(getintensity(med_seg))
+mean(med_seg::MedicalSegment) = mean(intensity(med_seg))
 " Gets the intensity standard deviation of an image segment"
-std(med_seg::MedicalSegment) = std(getintensity(med_seg))
+std(med_seg::MedicalSegment) = std(intensity(med_seg))
 " Gets the variation coefficient of an image segment"
 varcoef(med_seg::MedicalSegment) = getstd(med_seg) / getmean(med_seg)
 
--- test/Interfaces/images.jl
+++ test/Interfaces/images.jl
@@ -3,15 +3,63 @@
 ################
 
 # Using internal packages to test 
+using Apolo:create_grid,dimension
 using Apolo.Images
-using Apolo.ForwardProblem: getdim
 
 
 # Using external packages to test 
 using Test: @test, @testset
+using AutoHashEquals
 using HistogramThresholding: build_histogram
 using Statistics:  var, mean
 using DICOM: @tag_str
+
+
+@testset "Generic images" begin
+    
+    # 2D image 
+    # image dimensions 
+    spacing_img = (.5, .25)
+    num_pixels = (4, 3)
+    start_img = (1.,1.)
+    image_dimension = length(spacing_img)
+    finish_img = start_img .+ num_pixels .* spacing_img
+    length_img = finish_img .- start_img
+    # image intensity
+    intensity_vec = collect(1:12)
+    intensity_mat = reshape(intensity_vec, num_pixels )
+    # create the image grid
+    grid_img = create_img_grid(start_img, spacing_img, length_img, num_pixels)
+
+    # generate image 
+    generic_img = GenericImage(intensity_mat, num_pixels, spacing_img, start_img)
+
+    # test extractors
+    @test intensity(generic_img) == intensity_mat
+    @test intensity_type(generic_img) == eltype(intensity_mat)
+    @test dimension(generic_img) == image_dimension
+    @test numpix(generic_img) == num_pixels
+    @test spacing(generic_img) == spacing_img
+    @test start(generic_img) == start_img
+    @test finish(generic_img) == finish_img
+    @test length(generic_img) == length_img
+
+    # test coordinates generator
+    coords = coordinates(generic_img)
+    @test spacing_img[1] ==  coords[1][2] - coords[1][1] 
+    @test spacing_img[2] ==  coords[2][2] - coords[2][1] 
+    extremaᵢ, extremaⱼ = extrema.(coords)
+    @test extremaᵢ == (start_img[1] + spacing_img[1]/2, finish_img[1] -  spacing_img[1]/2)
+    @test extremaⱼ == (start_img[2] + spacing_img[2]/2, finish_img[2] -  spacing_img[2]/2)
+
+    # test mesh generator
+    grid_img_extracted = getgrid(generic_img)
+    @test grid_img.cells == grid_img_extracted.cells
+    @test grid_img.nodes == grid_img_extracted.nodes
+    @test grid_img.facesets == grid_img_extracted.facesets
+
+end
+
 
 
 # Constant variables and tpyes
@@ -26,13 +74,13 @@ const DCM_TO_LOAD = "./DICOMImages/"
 
     # get image dimensions
     dim = getdim(dcm);
-    dims = (256, 256, 400)
-    named_dims = (sagital = dims[1], coronal = dims[2], axial = dims[3])
-    @test dim == named_dims
+    num_pix = (256, 256, 400)
+    named_num_pix = (sagital = num_pix[1], coronal = num_pix[2], axial = num_pix[3])
+    @test dim == named_num_pix
 
     # get image intensity array
     intensity = getintensity(dcm)
-    @test size(intensity) == dims
+    @test size(intensity) == num_pix
 
     # get the image spacing
     spacing = (0.78125, 0.78125, 0.500003) 
@@ -88,3 +136,5 @@ end
     std_segments = std.(segments)
 
 end
+
+
