303021a8f59be09291136f440800905dd44a72be
mvanzulli <mvanzulli@fing.edu.uy>
2022-11-04 13:40:29 -0300

    deleted  from built in functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using SafeTestsets
     include("Interfaces/grids.jl")
 end
 
-# @safetestset "Apolo.Materials" begin
-#     include("Interfaces/materials.jl")
-# end
+@safetestset "Apolo.Materials" begin
+    include("Interfaces/materials.jl")
+end
 
 # @safetestset "Apolo.ForwardProblem" begin
 #     include("Interfaces/forward_problem.jl")
